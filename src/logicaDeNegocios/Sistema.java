dc4fa896a2777224111d5f578fd42d69786ef5c3
arielmmonestel <arielmmonestel@gmail.com>
2014-11-08 13:03:26 -0600

    Arregle conflictos

--- src/logicaDeNegocios/Sistema.java
+++ src/logicaDeNegocios/Sistema.java
@@ -264,6 +264,63 @@ public class Sistema {
 	
 }
 	
+
+	
+
+    public static void enviarMailConAdjunto(String correoDestinatario,String subject, String mensaje,String imagen)
+    {
+        try
+        {
+          // se obtiene el objeto Session. La configuración es para
+          // una cuenta de gmail.
+            Properties props = new Properties();
+            props.put("mail.smtp.host", "smtp.gmail.com");
+            props.setProperty("mail.smtp.starttls.enable", "true");
+            props.setProperty("mail.smtp.port", "587");
+            props.setProperty("mail.smtp.user", "alejandrinabiblioteca172@gmail.com");
+            props.setProperty("mail.smtp.auth", "true");
+            props.put("mail.smtp.ssl.trust", "smtp.gmail.com");//Linea que falta
+			 
+            Session session = Session.getDefaultInstance(props, null);
+            // session.setDebug(true);
+
+            // Se compone la parte del texto
+            BodyPart texto = new MimeBodyPart();
+            texto.setText(mensaje);
+
+            // Se compone el adjunto con la imagen
+            BodyPart adjunto = new MimeBodyPart();
+            adjunto.setDataHandler(
+                new DataHandler(new FileDataSource(imagen)));
+            adjunto.setFileName("fotoMascota.jpg");
+
+            // Una MultiParte para agrupar texto e imagen.
+            MimeMultipart multiParte = new MimeMultipart();
+            multiParte.addBodyPart(texto);
+            multiParte.addBodyPart(adjunto);
+
+            // Se compone el correo, dando to, from, subject y el
+            // contenido.
+            MimeMessage message = new MimeMessage(session);
+            message.setFrom(new InternetAddress("alejandrinabiblioteca172@gmail.com"));
+            message.addRecipient(
+                Message.RecipientType.TO,
+                new InternetAddress(correoDestinatario));
+            message.setSubject(subject);
+            message.setContent(multiParte);
+
+            // Se envia el correo.
+            Transport t = session.getTransport("smtp");
+            t.connect("alejandrinabiblioteca172@gmail.com", "alejandrina123"); 
+            t.sendMessage(message, message.getAllRecipients());
+            t.close();
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+	
 	private static boolean estaEnLista(ArrayList<String> lista, String dato){
 		/*verifica si dato está en lista*/
 		for(String str: lista)
@@ -669,60 +726,5 @@ public class Sistema {
 	///////////////////////////////////////// Adopciones //////////////////////////////////////////////////////
 	
 
-	
-
-    public static void enviarMailConAdjunto(String correoDestinatario,String subject, String mensaje,String imagen)
-    {
-        try
-        {
-          // se obtiene el objeto Session. La configuración es para
-          // una cuenta de gmail.
-            Properties props = new Properties();
-            props.put("mail.smtp.host", "smtp.gmail.com");
-            props.setProperty("mail.smtp.starttls.enable", "true");
-            props.setProperty("mail.smtp.port", "587");
-            props.setProperty("mail.smtp.user", "alejandrinabiblioteca172@gmail.com");
-            props.setProperty("mail.smtp.auth", "true");
-            props.put("mail.smtp.ssl.trust", "smtp.gmail.com");//Linea que falta
-			 
-            Session session = Session.getDefaultInstance(props, null);
-            // session.setDebug(true);
-
-            // Se compone la parte del texto
-            BodyPart texto = new MimeBodyPart();
-            texto.setText(mensaje);
-
-            // Se compone el adjunto con la imagen
-            BodyPart adjunto = new MimeBodyPart();
-            adjunto.setDataHandler(
-                new DataHandler(new FileDataSource(imagen)));
-            adjunto.setFileName("fotoMascota.jpg");
-
-            // Una MultiParte para agrupar texto e imagen.
-            MimeMultipart multiParte = new MimeMultipart();
-            multiParte.addBodyPart(texto);
-            multiParte.addBodyPart(adjunto);
-
-            // Se compone el correo, dando to, from, subject y el
-            // contenido.
-            MimeMessage message = new MimeMessage(session);
-            message.setFrom(new InternetAddress("alejandrinabiblioteca172@gmail.com"));
-            message.addRecipient(
-                Message.RecipientType.TO,
-                new InternetAddress(correoDestinatario));
-            message.setSubject(subject);
-            message.setContent(multiParte);
-
-            // Se envia el correo.
-            Transport t = session.getTransport("smtp");
-            t.connect("alejandrinabiblioteca172@gmail.com", "alejandrina123"); 
-            t.sendMessage(message, message.getAllRecipients());
-            t.close();
-        }
-        catch (Exception e)
-        {
-            e.printStackTrace();
-        }
-    }
 	
 }
