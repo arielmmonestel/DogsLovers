caa78933c130cdbe644ace63440575c63c3eba03
arielmmonestel <arielmmonestel@gmail.com>
2014-11-10 14:28:30 -0600

    Quienes Somos? - ok

--- src/acceso/Loggin.java
+++ src/acceso/Loggin.java
@@ -141,6 +141,32 @@ public class Loggin extends JFrame {
 			}
 		});
 		
+		JButton btnquinesSomos = new JButton("\u00BFQui\u00E9nes Somos?");
+		btnquinesSomos.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				VentanaInformacionDL.main(null);
+			}
+		});
+		btnquinesSomos.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseExited(MouseEvent arg0) {
+				btnquinesSomos.setForeground(Color.WHITE);
+			}
+			@Override
+			public void mouseEntered(MouseEvent arg0) {
+				btnquinesSomos.setForeground(Color.BLUE);
+			}
+		});
+		btnquinesSomos.setForeground(Color.WHITE);
+		btnquinesSomos.setFont(new Font("Khmer UI", Font.BOLD, 11));
+		btnquinesSomos.setFocusable(false);
+		btnquinesSomos.setFocusTraversalKeysEnabled(false);
+		btnquinesSomos.setFocusPainted(false);
+		btnquinesSomos.setContentAreaFilled(false);
+		btnquinesSomos.setBorderPainted(false);
+		btnquinesSomos.setBounds(414, 0, 138, 23);
+		contentPane.add(btnquinesSomos);
+		
 		btnIngresar.setBounds(154, 302, 245, 62);
 		contentPane.add(btnIngresar);
 		
---
+++ src/acceso/VentanaInformacionDL.java
@@ -0,0 +1,66 @@
+package acceso;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.Toolkit;
+import java.awt.Color;
+import javax.swing.JLabel;
+import javax.swing.ImageIcon;
+import javax.swing.SwingConstants;
+import java.awt.Font;
+import javax.swing.JTextPane;
+import javax.swing.JTextArea;
+
+public class VentanaInformacionDL extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					VentanaInformacionDL frame = new VentanaInformacionDL();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public VentanaInformacionDL() {
+		setTitle("\u00BFQui\u00E9nes Somos?");
+		setIconImage(Toolkit.getDefaultToolkit().getImage("./imgs/Icono.png"));
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		setBounds(100, 100, 515, 345);
+		contentPane = new JPanel();
+		contentPane.setBackground(new Color(99,84, 65));
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel labelFondo = new JLabel("");
+		labelFondo.setHorizontalAlignment(SwingConstants.CENTER);
+		labelFondo.setIcon(new ImageIcon("C:\\Users\\Ariel\\Documents\\GitHub\\DogsLovers\\imgs\\Icono Borroso.png"));
+		labelFondo.setBounds(362, 180, 127, 109);
+		contentPane.add(labelFondo);
+		
+		JTextPane txtpnsomosEstudiantesDe = new JTextPane();
+		txtpnsomosEstudiantesDe.setFont(new Font("Khmer UI", Font.BOLD, 14));
+		txtpnsomosEstudiantesDe.setForeground(Color.WHITE);
+		txtpnsomosEstudiantesDe.setText("      Somos  estudiantes de la carrera de Ingenier\u00EDa en Computaci\u00F3n  del Instituto Tecnol\u00F3gico de Costa Rica. \r\n      Esta aplicaci\u00F3n de escritorio es  creada para el proyecto n\u00FAmero tres del curso de Programaci\u00F3n Orientada a Objetos.\r\n      Deseamos que sea una herramienta para todas aquellas personas amantes de los animales, deseosas de encontrar a sus mascotas o cooperar reportando las encontradas.\r\n      Si desea obtener m\u00E1s informaci\u00F3n sobre nosotros, lo puede hacer al correo electr\u00F3nico dogs1lovers@gmail.com.\r\n\r\n\r\nEquipo de Dogs Lovers.\r\n");
+		txtpnsomosEstudiantesDe.setBounds(10, 11, 395, 249);
+		txtpnsomosEstudiantesDe.setBackground(null);
+		contentPane.add(txtpnsomosEstudiantesDe);
+	}
+}
